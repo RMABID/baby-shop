cf7dc90aab30c59a83106b2ee1cbff515933b950
RMABID <mdabidaliimran@gmail.com>
2025-02-14 18:40:26 +0600

    firebase key secrute

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6M35qpujbCVLhkEgILmXBcDjQCGbF8wM",
-  authDomain: "baby-shop-4543e.firebaseapp.com",
-  projectId: "baby-shop-4543e",
-  storageBucket: "baby-shop-4543e.firebasestorage.app",
-  messagingSenderId: "783956449007",
-  appId: "1:783956449007:web:e3da9f50655af2ac85ad1a",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
